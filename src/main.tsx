a1e4b6ea433f7f8f66da2065a7cc0c1587041d13
NevesPNG68 <nevespng@gmail.com>
2026-08-18 15:52:41 -0300

    Exibe painel de métricas abaixo do app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import MetricsPanel from './MetricsPanel.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="min-h-screen bg-[#050505]">
+      <App />
+      <MetricsPanel />
+    </div>
   </StrictMode>,
 );
